6624de3ae7c8dcd62cbf86e609480f1cc905fc92
Chris Ellis <chris@ideasbynature.com>
2016-02-02 01:22:19 -0600

    trying express

--- beer.js
+++ beer.js
@@ -52,13 +52,24 @@ This bot demonstrates many of the core features of Botkit:
     -> http://howdy.ai/botkit
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 var Botkit = require('./lib/Botkit.js');
 var requestify = require('requestify'); 
 var Brauhaus = require('brauhaus');
 require('brauhaus-beerxml');
-
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.send('it is running\n'); }).listen(process.env.PORT || 5000);
 
 if (!process.env.token) {
   console.log('Error: Specify token in environment');

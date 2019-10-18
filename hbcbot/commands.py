4a0039b1201eeb18cdf75fc48dd8501fde478fbc
Jim Rollenhagen <jim@jimrollenhagen.com>
2019-10-17 20:10:37 -0400

    Use the cooler ABV calculator
    Thanks mutedog!

--- hbcbot/commands.py
+++ hbcbot/commands.py
@@ -5,7 +5,8 @@ def calc_abv(args):
     og = args[0]
     fg = args[1]
     try:
-        abv = (float(og) - float(fg)) * 131
+        abv = ((76.08 * (float(og)-float(fg)) / (1.775-float(og))) *
+               (float(fg) / 0.794))
         return 'ABV is %.1f' % abv
     except Exception:
         # not passed numbers

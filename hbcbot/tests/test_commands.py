0f82905a18d697fb5dd9a498a673608254564fc5
dibbs online <roach@dopegoat.com>
2020-01-25 18:02:05 +1000

    hydrometer temprature adjustment calculator

--- hbcbot/tests/test_commands.py
+++ hbcbot/tests/test_commands.py
@@ -8,3 +8,15 @@ class TestAbv(unittest.TestCase):
         abv = commands._abv(1.135, 1.045)
         abv = round(abv, 1)
         self.assertEqual(abv, 14.1)
+
+class TestBrix(unittest.TestCase):
+    def test_brix(self):
+        brix = commands._brix_to_og(31.2)
+        brix = round(brix, 3)
+        self.assertEqual(brix, 1.135)
+
+class TestHydrometer(unittest.TestCase):
+    def test_hyadj(self):
+        ag = commands._hydro_temp_adj(1.131, 90, 60)
+        ag = round(ag, 3)
+        self.assertEqual(ag, 1.135)
